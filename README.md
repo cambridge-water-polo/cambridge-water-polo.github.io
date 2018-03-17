e92b65009a313aa677cfe8bc5531d4fece44e054
Tyler Lee <tlee753@gmail.com>
2018-03-17 02:40:33 -0500

    update website

--- README.md
+++
@@ -1,2 +0,0 @@
-# cambridge-water-polo.github.io
-Cambridge Water Polo Website
---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/Github/cambridge-water-polo.github.io;
+
+git add . *;
+
+git commit -m "update website";
+
+git push;
+
+cd -;
